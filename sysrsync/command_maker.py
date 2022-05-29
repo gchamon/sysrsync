f505c69f9f5bb5f2cebb2f78621a4f5cca789461
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2022-05-29 15:41:59 -0300

    minor refactor rsh comamnd check for readability

--- sysrsync/command_maker.py
+++ sysrsync/command_maker.py
@@ -34,7 +34,7 @@ def get_rsync_command(source: str,
                           else [])
 
     rsh = (get_rsh_command(private_key, rsh_port, strict_host_key_checking)
-           if private_key is not None or rsh_port is not None or strict_host_key_checking is not None
+           if any((private_key, rsh_port, strict_host_key_checking))
            else [])
 
     if options is None:
